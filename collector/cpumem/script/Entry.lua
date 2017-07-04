11ff1268ba1c3c7058d20830d76d98ac114beb5b
jamunoz <jamunoz@itrsgroup.com>
2017-07-04 07:37:20 +0200

    Update cpumem bundle

--- collector/cpumem/script/Entry.lua
+++ collector/cpumem/script/Entry.lua
@@ -1,3 +1,5 @@
+local logger = require "common/Logger"
+
 local isRunning
 local counter
 local hostNameResult
@@ -9,11 +11,20 @@ local timeLoop
 local myResultMessage =
 	{
 
+		osInfo =
+			{
+				["name"] 					= nil,
+				["version"] 			= nil,
+				["description"] 	= nil,
+				["architecture"] 	= nil
+			},
+
 		testRunInfo =
 			{
 				["project"] 		= nil,
 				["testRunID"] 	= nil,
-				["valoVersion"] = nil
+				["valoVersion"] = nil,
+				["collectorVersion"] = nil
 			},
 
 		["counter"] 	= nil,
@@ -33,14 +44,38 @@ local myResultMessage =
 function OnBeforeStart(config)
 
 	--Code...
+
+	logger.level = config['logLevelBundle']
+
+	if (logger.level == nil or logger.level < 1 or logger.level > 6) then
+		logger.level = 4
+	end
+
 	timeLoop = config['timeLoopMilli']
 
 	myResultMessage["testRunInfo"]["project"]			= config['project']
 	myResultMessage["testRunInfo"]["testRunID"]  	= config['testRunID']
 	myResultMessage["testRunInfo"]["valoVersion"] = config['valoVersion']
 
+	bundleInfo = Collector.Core.getBundleInformation()
+	if bundleInfo[0] == 0 then
+		myResultMessage["testRunInfo"]["collectorVersion"] = bundleInfo[1]["collector-version"]
+	else
+		logger.err("[PROCESS][getBundleInformation] error while calling method ('" .. bundleInfo[1] .. "')")
+	end
+
+	systemInfo = Collector.Core.getSystemInformation()
+	if systemInfo[0] == 0 then
+		myResultMessage["osInfo"]["name"] = systemInfo[1]["os-name"]
+		myResultMessage["osInfo"]["description"] = systemInfo[1]["os-description"]
+		myResultMessage["osInfo"]["version"] = systemInfo[1]["os-version"]
+		myResultMessage["osInfo"]["architecture"] = systemInfo[1]["architecture"]
+	else
+		logger.err("[PROCESS][getSystemInformation] error while calling method ('" .. systemInfo[1] .. "')")
+	end
+
 	isRunning = true
-	print("The cpumem bundle is running")
+	logger.info("[CPUMEM][OnBeforeStart] The cpumem bundle is running on " .. systemInfo[1]["os-name"])
 
 end
 
@@ -48,37 +83,45 @@ function OnStart()
 
 	counter = 1
 
-	while (isRunning)  do
+	hostNameResult = Collector.Network.getHostName()
 
-		hostNameResult = Collector.Network.getHostName()
-		cpuResult = Collector.Process.getGlobalCpuUsage()
-		memoryResult = Collector.Process.getGlobalMemoryUsage()
+	if hostNameResult[0] == 0 then
 
-		myResultMessage["counter"] 		= counter
-		myResultMessage["timestamp"] 	= (os.time()*1000)
-		myResultMessage["hostname"] 	= hostNameResult[1]
-		myResultMessage["cpu_global_percent"] = cpuResult * 100
-		myResultMessage["mem_global_percent"] = memoryResult[1]["memory-usage"] * 100
-		myResultMessage["mem_total_physical"] = memoryResult[1]["total-physical"]
-		myResultMessage["mem_free_physical"] 	= memoryResult[1]["free-physical"]
-		myResultMessage["mem_total_paging"] 	= memoryResult[1]["total-paging"]
-		myResultMessage["mem_free_paging"] 		= memoryResult[1]["free-paging"]
-		myResultMessage["mem_total_virtual"]	= memoryResult[1]["total-virtual"]
-		myResultMessage["mem_free_virtual"] 	= memoryResult[1]["free-virtual"]
-		myResultMessage["mem_free_extended"] 	= memoryResult[1]["free-extended"]
+		while (isRunning)  do
 
-		result = Collector.Connector.send("MSG_CPUMEM", myResultMessage)
-		if result[0] ~= 0 then
-			print("error while calling method ('" .. result[1] .. "')")
+			cpuResult = Collector.Process.getGlobalCpuUsage()
+			memoryResult = Collector.Process.getGlobalMemoryUsage()
+
+			myResultMessage["counter"] 		= counter
+			myResultMessage["timestamp"] 	= (os.time()*1000)
+			myResultMessage["hostname"] 	= hostNameResult[1]
+			myResultMessage["cpu_global_percent"] = cpuResult * 100
+			myResultMessage["mem_global_percent"] = memoryResult[1]["memory-usage"] * 100
+			myResultMessage["mem_total_physical"] = memoryResult[1]["total-physical"]
+			myResultMessage["mem_free_physical"] 	= memoryResult[1]["free-physical"]
+			myResultMessage["mem_total_paging"] 	= memoryResult[1]["total-paging"]
+			myResultMessage["mem_free_paging"] 		= memoryResult[1]["free-paging"]
+			myResultMessage["mem_total_virtual"]	= memoryResult[1]["total-virtual"]
+			myResultMessage["mem_free_virtual"] 	= memoryResult[1]["free-virtual"]
+			myResultMessage["mem_free_extended"] 	= memoryResult[1]["free-extended"]
+
+			result = Collector.Connector.send("MSG_CPUMEM", myResultMessage)
+			if result[0] ~= 0 then
+				logger.err("[CPUMEM][send] error while calling method ('" .. result[1] .. "')")
+			end
+
+			Collector.DateTime.sleepMilliseconds(timeLoop)
+			counter = counter+1
+
 		end
 
-		Collector.DateTime.sleepMilliseconds(timeLoop)
-		counter = counter+1
-
+	else
+		logger.err("[CPUMEM][getHostName] error while calling method ('" .. hostNameResult[1] .. "')")
 	end
 
 end
 
 function OnBeforeStop()
 	isRunning = false
+	logger.info("[CPUMEM][OnBeforeStop] The cpumem bundle is closing")
 end
